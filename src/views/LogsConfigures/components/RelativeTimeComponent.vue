f5bc7c7c07317634ec195f27d39a18ce690e6ce4
Gigi <gigichen380@gmail.com>
2025-11-07 15:41:53 +0800

    feat(logs): 相对时间

--- src/views/LogsConfigures/components/RelativeTimeComponent.vue
+++ src/views/LogsConfigures/components/RelativeTimeComponent.vue
@@ -1,7 +1,8 @@
 <script setup lang="tsx">
 import { useTimeShortcuts } from './useTimeShortcuts'
-import { TimeList } from '@/api/logsConfigure/types'
-import { ref, onMounted, reactive, nextTick } from 'vue'
+import { Shortcut, TimeList } from '@/api/logsConfigure/types'
+import { ref, watch, reactive, nextTick } from 'vue'
+import { formatToDateTime, dateUtil } from '@/utils/dateUtil'
 import { ElSwitch } from 'element-plus'
 const exactHour = ref(false)
 const minuteList: TimeList[] = [
@@ -22,31 +23,44 @@ const dayList: TimeList[] = [
   { text: '近3天', msOffset: 3 * 24 * 60 * 60 * 1000 },
   { text: '近7天', msOffset: 7 * 24 * 60 * 60 * 1000 }
 ]
-const firstList: TimeList[] = [
+const nowCutList: TimeList[] = [
   { text: '今天', msOffset: 1 * 24 * 60 * 60 * 1000 },
   { text: '本周', msOffset: 3 * 24 * 60 * 60 * 1000 },
   { text: '本月', msOffset: 7 * 24 * 60 * 60 * 1000 }
 ]
-const secondList: TimeList[] = [
+const lastCutList: TimeList[] = [
   { text: '昨天', msOffset: 1 * 24 * 60 * 60 * 1000 },
   { text: '前天', msOffset: 3 * 24 * 60 * 60 * 1000 },
   { text: '上周', msOffset: 7 * 24 * 60 * 60 * 1000 }
 ]
 
-const { fixedShortcuts: minuteShortcuts } = useTimeShortcuts(minuteList)
-const { fixedShortcuts: hourShortcuts } = useTimeShortcuts(hourList)
-const { fixedShortcuts: dayShortcuts } = useTimeShortcuts(dayList)
+const { fixedShortcuts: minuteShortcuts } = useTimeShortcuts(minuteList, exactHour)
+const { fixedShortcuts: hourShortcuts } = useTimeShortcuts(hourList, exactHour)
+const { fixedShortcuts: dayShortcuts } = useTimeShortcuts(dayList, exactHour)
+const { fixedShortcuts: nowShortcuts } = useTimeShortcuts(nowCutList, exactHour)
+const { fixedShortcuts: lastShortcuts } = useTimeShortcuts(lastCutList, exactHour)
 const firstShortcuts = [minuteShortcuts, hourShortcuts, dayShortcuts]
-const secondShortcuts = [firstList, secondList]
+const secondShortcuts = [nowShortcuts, lastShortcuts]
+const selectedShortcut = ref<Shortcut | null>(minuteShortcuts[0])
+const range = ref<[Date, Date]>(selectedShortcut.value?.value() ?? [new Date(), new Date()])
+const handleClick = (short) => {
+  selectedShortcut.value = short
+  range.value = short.value()
+}
+
+watch(exactHour, () => {
+  if (selectedShortcut.value) {
+    range.value = selectedShortcut.value.value()
+  }
+})
 </script>
 
 <template>
   <div>
     <div class="flex justify-between items-center">
-      <span>时间</span>
+      <span>{{ formatToDateTime(range[0]) }}~{{ formatToDateTime(range[1]) }}</span>
       <div>
-        整点时间
-        <ElSwitch v-model="exactHour" />
+        <ElSwitch v-model="exactHour" active-text="整点时间" />
       </div>
     </div>
     <div class="grid grid-cols-[3fr_1fr]">
@@ -56,7 +70,12 @@ const secondShortcuts = [firstList, secondList]
           v-for="(item, index) in firstShortcuts"
           :key="index"
         >
-          <span v-for="short in item" :key="short.text">
+          <span
+            v-for="short in item"
+            :key="short.text"
+            @click="handleClick(short)"
+            class="cursor-pointer"
+          >
             {{ short.text }}
           </span>
         </div>
@@ -67,7 +86,12 @@ const secondShortcuts = [firstList, secondList]
           v-for="(item, index) in secondShortcuts"
           :key="index"
         >
-          <span v-for="short in item" :key="short.text">
+          <span
+            v-for="short in item"
+            :key="short.text"
+            @click="handleClick(short)"
+            class="cursor-pointer"
+          >
             {{ short.text }}
           </span>
         </div>
--- src/views/LogsConfigures/components/useTimeShortcuts.tsx
+++ src/views/LogsConfigures/components/useTimeShortcuts.tsx
@@ -3,22 +3,20 @@ import { ref } from 'vue'
 
 import { Shortcut, TimeList } from '@/api/logsConfigure/types'
 
-export const useTimeShortcuts = (fixedList: TimeList[]) => {
+export const useTimeShortcuts = (fixedList: TimeList[], exactHour = ref(false)) => {
   // 工具函数：从毫秒偏移量生成时间区间
   const makeRange = (msOffset: number): [Date, Date] => {
-    const end = new Date()
+    const now = new Date()
+    let end = new Date(now.getTime())
+
+    // 对于分钟级别，不整点
+    if (exactHour.value && msOffset >= 60 * 60 * 1000) {
+      end.setMinutes(0, 0, 0) // 整点对齐
+    }
+
     const start = new Date(end.getTime() - msOffset)
     return [start, end]
   }
-  /**
- * [
-    { text: '最近1分钟', value: () => makeRange(1 * 60 * 1000) },
-    { text: '最近5分钟', value: () => makeRange(5 * 60 * 1000) },
-    { text: '最近1小时', value: () => makeRange(60 * 60 * 1000) },
-    { text: '最近7天', value: () => makeRange(7 * 24 * 60 * 60 * 1000) },
-    { text: '最近30天', value: () => makeRange(30 * 24 * 60 * 60 * 1000) }
-  ]
- */
   // 固定选项
   const fixedShortcuts: Shortcut[] = fixedList.map((item) => ({
     text: item.text,
